787c9e6f69803cfa42a4796697face30882e2da7
rob owens <rtowens17@gmail.com>
2023-03-22 00:45:08 -0500

    feat: add capability to setup theme

--- Installation/mint-setup/setup.sh
+++ Installation/mint-setup/setup.sh
@@ -1,3 +1,18 @@
 # Setup VSCode settings link to use this repository
 ln -s "$(pwd)/.vscode/settings.json" "${HOME}/.config/VSCodium/User/settings.json"
 ln -s "$(pwd)/.vscode/keybindings.json" "${HOME}/.config/VSCodium/User/keybindings.json"
+
+# Install Catppucin Theme
+unzip "$(pwd)/packages/Themes/Catppuccin-Mocha-Standard-Maroon-Dark.zip" -d "${HOME}/.themes"
+ln -s "${HOME}/.themes/Catppuccin-Mocha-Standard-Maroon-Dark/gtk-4.0/assets" "${HOME}/.config/gtk-4.0/assets"
+ln -s "${HOME}/.themes/Catppuccin-Mocha-Standard-Maroon-Dark/gtk-4.0/gtk.css" "${HOME}/.config/gtk-4.0/gtk.css"
+ln -s "${HOME}/.themes/Catppuccin-Mocha-Standard-Maroon-Dark/gtk-4.0/gtk-dark.css" "${HOME}/.config/gtk-4.0/gtk-dark.css"
+
+# Install Candy Icon Theme
+mkdir "${HOME}/.icons"
+unzip "$(pwd)/packages/Themes/candy-icons-master.zip" -d "${HOME}/.icons"
+
+# Configure to use new themes
+
+gsettings set org.gnome.desktop.interface icon-theme candy-icons-master
+gsettings set org.gnome.desktop.interface gtk-theme Catppuccin-Mocha-Standard-Maroon-Dark
