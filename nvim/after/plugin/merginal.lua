dcf2a8cba4a954aa79e4e3136de2d62ae98c879c
Rob Owens <rob.owens@rsmus.com>
2023-07-11 08:01:01 -0500

    remove project manager

---
+++ nvim/after/plugin/merginal.lua
@@ -0,0 +1,1 @@
+vim.keymap.set('n', '<leader>gb', ':MerginalToggle<cr>', { desc = 'Git Branches' })
--- nvim/after/plugin/project.lua
+++
@@ -1,8 +0,0 @@
-require('project_nvim').setup {
-    ignore_lsp = { 'lua_ls' },
-    silent_chdir = false
-}
-
-require('telescope').load_extension('projects')
-
-vim.keymap.set('n', '<leader>fr', ':Telescope projects<cr>', { desc = 'Repo search' })
