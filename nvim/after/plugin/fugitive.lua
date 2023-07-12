58a15f0b9692b59341298c2922ede2e6a597c2f5
Rob Owens <rtowens17@gmail.com>
2023-07-12 14:27:38 -0500

    refactor git fugitive functions

--- nvim/after/plugin/fugitive.lua
+++ nvim/after/plugin/fugitive.lua
@@ -1,19 +1,40 @@
-vim.keymap.set('n', '<leader>gs', function ()
-    local windows = vim.api.nvim_list_wins()
-    for _, handle in ipairs(windows) do
-        local s, _ = pcall(vim.api.nvim_win_get_var, handle, 'fugitive_status')
-        if s then
-            vim.api.nvim_win_close(handle, true)
-            return
-        end
-    end
-    vim.cmd(':vertical Git')
-end, { desc = 'Open Git Status'})
 vim.keymap.set('n', '<leader>gp', ':Git push --quiet<cr>', { desc = 'Git Push' })
 vim.keymap.set('n', '<leader>gu', ':Git pull<cr>', { desc = 'Git Pull' })
 
+local function findGitWindow()
+    local windows = vim.api.nvim_list_wins()
+    for _, handle in ipairs(windows) do
+        local s, _ = pcall(vim.api.nvim_win_get_var, handle, 'fugitive_status')
+        if s and vim.api.nvim_win_is_valid(handle) then
+            return handle
+        end
+    end
+    return false
+end
+
+local function openGit()
+    vim.cmd(':Git!') -- Open small window of git at the below
+end
+
+local function closeGit()
+    local gitWindow = findGitWindow()
+    if gitWindow ~= nil then
+        vim.api.nvim_win_close(gitWindow, true)
+    end
+    return gitWindow ~= nil
+end
+
+local function toggleGit()
+    local gitWindow = findGitWindow()
+    if gitWindow == false then
+        openGit()
+    else
+        -- closeGit()
+        vim.api.nvim_win_close(gitWindow, true)
+    end
+end
+
 local function gitCommit()
-    vim.cmd(':Git diff --cached')
     local message = vim.fn.input('Message > ')
     if message ~= nil and message ~= '' then
         vim.cmd(':Git commit -m "' .. message .. '"')
@@ -22,6 +43,7 @@ local function gitCommit()
     end
 end
 
+vim.keymap.set('n', '<leader>gs', toggleGit, { desc = 'Open Git Status'})
 vim.keymap.set('n', '<leader>gc', gitCommit, { desc = 'Git Commit' })
 vim.keymap.set('n', '<leader>gd', ':vert Gdiff HEAD<cr>', { desc = 'Git Diff current file' })
 vim.keymap.set('n', '<leader>gf', function ()
