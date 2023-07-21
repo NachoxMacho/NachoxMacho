9d4e642a58567b2fc7ecd5a0c51f09c282d8687e
Rob Owens <rtowens17@gmail.com>
2023-07-20 20:44:26 -0500

    update toggle term keymap to C-j

--- nvim/after/plugin/toggleterm.lua
+++ nvim/after/plugin/toggleterm.lua
@@ -4,5 +4,6 @@ require('toggleterm').setup{
     persist_mode = false,
 }
 
-vim.keymap.set('n', '<leader>t', ':ToggleTerm<cr>')
+vim.keymap.set('n', '<C-j>', ':ToggleTerm<cr>')
+vim.keymap.set('t', '<C-j>', '<C-\\><C-n>:ToggleTerm<cr>')
 vim.keymap.set('t', '<Esc>', '<C-\\><C-n>:ToggleTerm<cr>')
