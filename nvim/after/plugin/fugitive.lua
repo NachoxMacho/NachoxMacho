1f424e83f608b97ffe5e4f24ab74b27d0ff7811f
Rob Owens <rob.owens@rsmus.com>
2023-07-21 08:23:34 -0500

    general formatting for fugitive

--- nvim/after/plugin/fugitive.lua
+++ nvim/after/plugin/fugitive.lua
@@ -60,17 +60,16 @@ local function gitAddCurrentFile()
 end
 
 
-vim.keymap.set('n', '<leader>gs', toggleGit, { desc = 'Open Git Status'})
+vim.keymap.set('n', '<leader>gs', toggleGit, { desc = 'Open Git Status' })
 vim.keymap.set('n', '<leader>ga', gitAddCurrentFile, { desc = 'Add current file' })
 vim.keymap.set('n', '<leader>gc', gitCommit, { desc = 'Commit' })
 vim.keymap.set('n', '<leader>gd', ':vert Gdiff HEAD<cr>', { desc = 'Diff current file' })
-vim.keymap.set('n', '<leader>gg', function ()
+vim.keymap.set('n', '<leader>gg', function()
     gitAddCurrentFile()
     if gitCommit() then
         vim.cmd(':Git push')
     end
 end, { desc = 'Stage current file and commit' })
-vim.keymap.set('n', '<leader>gb', ':Telescope git_branches<cr>', { desc = 'Switch Branch'})
-vim.keymap.set('n', '<leader>gl', ':Telescope git_commits<cr>', { desc = 'Search Commit Log'})
-vim.keymap.set('n', '<leader>gt', gitCacheView, { desc = 'Preview commit'})
-
+vim.keymap.set('n', '<leader>gb', ':Telescope git_branches<cr>', { desc = 'Switch Branch' })
+vim.keymap.set('n', '<leader>gl', ':Telescope git_commits<cr>', { desc = 'Search Commit Log' })
+vim.keymap.set('n', '<leader>gt', gitCacheView, { desc = 'Preview commit' })
