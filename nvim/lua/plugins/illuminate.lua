4abfc54f854bd546980ada7c63ed7f504eedecdd
Rob Owens <rtowens17@gmail.com>
2023-07-22 16:38:42 -0500

    on load illuminate on file open

--- nvim/lua/plugins/illuminate.lua
+++ nvim/lua/plugins/illuminate.lua
@@ -1,5 +1,6 @@
 return {
     'RRethy/vim-illuminate',
+    event = { "BufReadPost", "BufNewFile" },
     config = function ()
         require('illuminate').configure({ under_cursor = true })
     end
