71a751bad38bf92e767a53cf302b90d85cc7fd81
Rob Owens <rtowens17@gmail.com>
2023-07-22 16:37:59 -0500

    move harpoon to lazy load and set keymaps

--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -1,22 +1,20 @@
+local mark = require("harpoon.mark")
+local ui = require("harpoon.ui")
+
 return {
     'theprimeagen/harpoon',
-    lazy = true,
-    config = function ()
-        
-        -- local mark = require("harpoon.mark")
-        -- local ui = require("harpoon.ui")
+    keys = {
+        { "<leader>aj", function() mark.set_current_at(1) end, desc = 'Set Harpoon 1' },
+        { "<leader>ak", function() mark.set_current_at(2) end, desc = 'Set Harpoon 2' },
+        { "<leader>al", function() mark.set_current_at(3) end, desc = 'Set Harpoon 3' },
+        { "<leader>a;", function() mark.set_current_at(4) end, desc = 'Set Harpoon 4' },
 
-        -- vim.keymap.set("n", "<leader>aj", function() mark.set_current_at(1) end)
-        -- vim.keymap.set("n", "<leader>ak", function() mark.set_current_at(2) end)
-        -- vim.keymap.set("n", "<leader>al", function() mark.set_current_at(3) end)
-        -- vim.keymap.set("n", "<leader>a;", function() mark.set_current_at(4) end)
-        -- vim.keymap.set("n", "<leader>r", ui.toggle_quick_menu)
-
-        -- vim.keymap.set("n", "<leader>j", function() ui.nav_file(1) end, { desc = 'Harpoon 1' })
-        -- vim.keymap.set("n", "<leader>k", function() ui.nav_file(2) end, { desc = 'Harpoon 2' })
-        -- vim.keymap.set("n", "<leader>l", function() ui.nav_file(3) end, { desc = 'Harpoon 3' })
-        -- vim.keymap.set("n", "<leader>;", function() ui.nav_file(4) end, { desc = 'Harpoon 4' })
-    end
+        { "<leader>r", ui.toggle_quick_menu, desc = 'Quick Menu' },
+        { "<leader>j", function() ui.nav_file(1) end, desc = 'Harpoon 1' },
+        { "<leader>k", function() ui.nav_file(2) end, desc = 'Harpoon 2' },
+        { "<leader>l", function() ui.nav_file(3) end, desc = 'Harpoon 3' },
+        { "<leader>;", function() ui.nav_file(4) end, desc = 'Harpoon 4' },
+    }
 }
 
 
