b76f7caebfe3ce1d1ac3c2a8f0cdeb34ee1eedea
Rob Owens <rtowens17@gmail.com>
2023-07-22 23:08:21 -0500

    remove autoclose plugin, see mini.pais

--- nvim/lua/plugins/autoclose.lua
+++
@@ -1,8 +0,0 @@
-return {
-    'm4xshen/autoclose.nvim',
-    opts = {
-        keys = {
-            ["<"] = { escape = true, close = true, pair = "<>", disabled_filetypes = {} },
-        }
-    }
-}
