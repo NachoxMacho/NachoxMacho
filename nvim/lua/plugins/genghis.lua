3b1ced15fb845f90858951371abae3faefa247f1
Rob Owens <rtowens17@gmail.com>
2023-07-22 23:06:10 -0500

    add genghis keymaps and autocomplete option

--- nvim/lua/plugins/genghis.lua
+++ nvim/lua/plugins/genghis.lua
@@ -1,7 +1,22 @@
 return {
     'chrisgrieser/nvim-genghis',
-    dependencies = { 'stevearc/dressing.nvim' },
+    dependencies = {
+        'stevearc/dressing.nvim',
+        'hrsh7th/nvim-cmp',
+        'hrsh7th/cmp-omni'
+    },
     keys = {
-        { '<leader>fc', ':New<cr>', desc = 'Create File' }
-    }
+        { '<leader>fc', ':New<cr>', desc = 'Create File' },
+        { '<leader>fm', ':Move<cr>', desc = 'Move File' },
+        { '<leader>fd', ':Duplicate<cr>', desc = 'Duplicate File' },
+        { '<leader>fx', ':Chmodx<cr>', desc = 'Chmod x File' },
+        { '<leader>fr', ':Trash<cr>', desc = 'Delete File' },
+    },
+    config = function ()
+        -- required setup for cmp, somewhere after your main cmp-config
+        local cmp = require('genghis')
+        cmp.setup.filetype('DressngInput', {
+            sources = cmp.confg.sources { {name = 'omni'} },
+        })
+    end
 }
