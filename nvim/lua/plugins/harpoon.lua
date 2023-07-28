b0e96fae8d0f5a2b290689ce51a5c2ce6538eb3a
Rob Owens <rob.owens@rsmus.com>
2023-07-28 13:49:23 +0000

    update harpoon to bootstrap

--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -1,21 +1,15 @@
-local mark = require("harpoon.mark")
-local ui = require("harpoon.ui")
-
 return {
     'theprimeagen/harpoon',
     keys = {
-        { "<leader>aj", function() mark.set_current_at(1) end, desc = 'Set Harpoon 1' },
-        { "<leader>ak", function() mark.set_current_at(2) end, desc = 'Set Harpoon 2' },
-        { "<leader>al", function() mark.set_current_at(3) end, desc = 'Set Harpoon 3' },
-        { "<leader>a;", function() mark.set_current_at(4) end, desc = 'Set Harpoon 4' },
+        { "<leader>aj", function() require('harpoon.mark').set_current_at(1) end, desc = 'Set Harpoon 1' },
+        { "<leader>ak", function() require('harpoon.mark').set_current_at(2) end, desc = 'Set Harpoon 2' },
+        { "<leader>al", function() require('harpoon.mark').set_current_at(3) end, desc = 'Set Harpoon 3' },
+        { "<leader>a;", function() require('harpoon.mark').set_current_at(4) end, desc = 'Set Harpoon 4' },
 
-        { "<leader>r", ui.toggle_quick_menu, desc = 'Quick Menu' },
-        { "<leader>j", function() ui.nav_file(1) end, desc = 'Harpoon 1' },
-        { "<leader>k", function() ui.nav_file(2) end, desc = 'Harpoon 2' },
-        { "<leader>l", function() ui.nav_file(3) end, desc = 'Harpoon 3' },
-        { "<leader>;", function() ui.nav_file(4) end, desc = 'Harpoon 4' },
+        { "<leader>r", function() require('harpoon.ui').toggle_quick_menu() end, desc = 'Quick Menu' },
+        { "<leader>j", function() require('harpoon.ui').nav_file(1) end, desc = 'Harpoon 1' },
+        { "<leader>k", function() require('harpoon.ui').nav_file(2) end, desc = 'Harpoon 2' },
+        { "<leader>l", function() require('harpoon.ui').nav_file(3) end, desc = 'Harpoon 3' },
+        { "<leader>;", function() require('harpoon.ui').nav_file(4) end, desc = 'Harpoon 4' },
     }
 }
-
-
-
