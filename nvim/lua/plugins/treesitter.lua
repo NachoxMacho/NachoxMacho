5533c4533c73d3c4b1cc7fede14ccea2b8243dcb
Rob Owens <rtowens17@gmail.com>
2023-07-29 22:16:27 -0500

    add tsx to treesitter languages

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return {
     build = ':TSUpdate',
     config = function ()
          require('nvim-treesitter.configs').setup({
-            ensure_installed = { 'vimdoc', 'javascript', 'typescript', 'c', 'lua' },
+            ensure_installed = { 'vimdoc', 'javascript', 'typescript', 'c', 'lua', 'tsx' },
             sync_install = false,
             auto_install = true,
             hightlight = {
