8475523c6ab3228fad9ce1aa8905be86984fbbfc
Rob Owens <rtowens17@gmail.com>
2023-07-29 22:01:27 -0500

    add gitsigns keymaps for condensed git modifications

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -2,6 +2,17 @@ return {
     'lewis6991/gitsigns.nvim',
     lazy = false,
     config = function ()
-        require('gitsigns').setup()
-    end
+        local gs = require('gitsigns')
+        gs.setup()
+        vim.keymap.set('n', '<leader>hs', gs.stage_hunk, { desc = 'Stage Chunk'})
+        vim.keymap.set('n', '<leader>hr', gs.reset_hunk)
+        vim.keymap.set('v', '<leader>hs', function() gs.stage_hunk {vim.fn.line('.'), vim.fn.line('v')} end)
+        vim.keymap.set('v', '<leader>hr', function() gs.reset_hunk {vim.fn.line('.'), vim.fn.line('v')} end)
+        vim.keymap.set('n', '<leader>hu', gs.undo_stage_hunk)
+        vim.keymap.set('n', '<leader>hR', gs.reset_buffer)
+        vim.keymap.set('n', '<leader>hp', gs.preview_hunk)
+        vim.keymap.set('n', '<leader>hb', function() gs.blame_line{full=true} end)
+        vim.keymap.set('n', '<leader>hB', gs.toggle_current_line_blame)
+        vim.keymap.set('n', '<leader>hd', gs.toggle_deleted)
+    end,
 }
