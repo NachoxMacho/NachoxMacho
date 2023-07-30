09865d462b3f131b73e2b893b17510fce96261fa
Rob Owens <rtowens17@gmail.com>
2023-07-29 22:10:00 -0500

    update fzf to only load if make is available

--- nvim/lua/plugins/fzf.lua
+++ nvim/lua/plugins/fzf.lua
@@ -1,14 +1,12 @@
-local makeCommand = 'make'
-if vim.fn.has('win64') == 1 or vim.fn.has('win32') == 1 or vim.fn.has('win16') == 1 then
-    makeCommand = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build'
-end
-
 return {
     'nvim-telescope/telescope-fzf-native.nvim',
     depdendencies = {
         'nvim-telescope/telescope.nvim',
     },
-    build = makeCommand,
+    build = 'make',
+    cond = function ()
+        return vim.fn.executable 'make' == 1
+    end,
     config = function ()
         require('telescope').setup({
             extensions = {
