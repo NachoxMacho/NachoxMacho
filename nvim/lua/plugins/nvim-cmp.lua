654a43d17b30ec022272eb50879a1863efb4bc59
Rob Owens <rtowens17@gmail.com>
2023-07-30 07:57:06 -0500

    fix lua snippets, and re-enable toggle code completion

--- nvim/lua/plugins/nvim-cmp.lua
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -17,10 +17,11 @@ return {
     },
     config = function()
         local cmp = require('cmp')
+        local luasnip = require('luasnip')
         cmp.setup({
             snippet = {
                 expand = function(args)
-                    require('luasnip').lsp_expand(args.body)
+                    luasnip.lsp_expand(args.body)
                 end
             },
             window = {
@@ -70,8 +71,9 @@ return {
             },
             sources = {
                 { name = 'nvim_lsp' },
+                { name = 'luasnip' },
                 { name = 'buffer' },
-                { name = 'path' }
+                { name = 'path' },
             },
             view = {
                 entries = 'custom',
@@ -81,8 +83,34 @@ return {
                 ['<down>'] = cmp.mapping.select_next_item(),
                 ['<up>'] = cmp.mapping.select_prev_item(),
                 ['<enter>'] = cmp.mapping.confirm({ select = true, behavior = cmp.ConfirmBehavior.Replace }),
-                ['<C-k>'] = cmp.mapping.complete(),
+                ['<C-k>'] = cmp.mapping({
+                    i = function(_)
+                        if cmp.visible() then
+                            cmp.close()
+                        else
+                            cmp.complete()
+                        end
+                    end
+                    }),
                 ['<C-f>'] = cmp.mapping.close(),
+                ['<Tab>'] = cmp.mapping(function(fallback)
+                    if cmp.visible() then
+                        cmp.select_next_item()
+                    elseif luasnip.expand_or_locally_jumpable() then
+                        luasnip.expand_or_jump()
+                    else
+                        fallback()
+                    end
+                end, { 'i', 's' }),
+                ['<S-Tab>'] = cmp.mapping(function(fallback)
+                    if cmp.visible() then
+                        cmp.select_prev_item()
+                    elseif luasnip.locally_jumpable(-1) then
+                        luasnip.jump(-1)
+                    else
+                        fallback()
+                    end
+                end, { 'i', 's' }),
             }
         })
     end
