69c4e7f9cb63e8c6532444053e622ea4d197ff04
Rob Owens <rtowens17@gmail.com>
2023-07-31 06:53:26 -0500

    add some nice options from kickstart.nvim

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -1,18 +1,31 @@
 return {
     'lewis6991/gitsigns.nvim',
     lazy = false,
-    config = function ()
+    config = function()
         local gs = require('gitsigns')
-        gs.setup()
-        vim.keymap.set('n', '<leader>hs', gs.stage_hunk, { desc = 'Stage Chunk'})
-        vim.keymap.set('n', '<leader>hr', gs.reset_hunk, { desc = 'Reset Chunk'})
-        vim.keymap.set('v', '<leader>hs', function() gs.stage_hunk {vim.fn.line('.'), vim.fn.line('v')} end, { desc = 'Stage Chunk'})
-        vim.keymap.set('v', '<leader>hr', function() gs.reset_hunk {vim.fn.line('.'), vim.fn.line('v')} end, { desc = 'Reset Chunk'})
+        gs.setup({
+            opts = {
+                -- See `:help gitsigns.txt`
+                signs = {
+                    add = { text = '+' },
+                    change = { text = '~' },
+                    delete = { text = '_' },
+                    topdelete = { text = '‾' },
+                    changedelete = { text = '~' },
+                }
+            }
+        })
+        vim.keymap.set('n', '<leader>hs', gs.stage_hunk, { desc = 'Stage Chunk' })
+        vim.keymap.set('n', '<leader>hr', gs.reset_hunk, { desc = 'Reset Chunk' })
+        vim.keymap.set('v', '<leader>hs', function() gs.stage_hunk { vim.fn.line('.'), vim.fn.line('v') } end,
+            { desc = 'Stage Chunk' })
+        vim.keymap.set('v', '<leader>hr', function() gs.reset_hunk { vim.fn.line('.'), vim.fn.line('v') } end,
+            { desc = 'Reset Chunk' })
         vim.keymap.set('n', '<leader>hu', gs.undo_stage_hunk, { desc = 'Unstage Chunk' })
         vim.keymap.set('n', '<leader>hR', gs.reset_buffer, { desc = 'Reset Buffer' })
         vim.keymap.set('n', '<leader>hp', gs.preview_hunk, { desc = 'Preview Chunk' })
-        vim.keymap.set('n', '<leader>hb', function() gs.blame_line{full=true} end, { desc = 'Blame' })
+        vim.keymap.set('n', '<leader>hb', function() gs.blame_line { full = true } end, { desc = 'Blame' })
         vim.keymap.set('n', '<leader>hB', gs.toggle_current_line_blame, { desc = 'Toggle inline blame' })
-        vim.keymap.set('n', '<leader>hd', gs.toggle_deleted, { desc = 'Toggle deleted'})
+        vim.keymap.set('n', '<leader>hd', gs.toggle_deleted, { desc = 'Toggle deleted' })
     end,
 }
--- nvim/lua/plugins/indent-blankline.lua
+++ nvim/lua/plugins/indent-blankline.lua
@@ -2,6 +2,7 @@ return {
     'lukas-reineke/indent-blankline.nvim',
     opts = {
         show_current_context = true,
-        show_current_context_start = true
+        show_current_context_start = true,
+        show_trailing_blankline_indent = false,
     }
 }
