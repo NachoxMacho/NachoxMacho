5b7b9d4af2d406ac845cd9b9601f098bf9eb352d
Rob Owens <rtowens17@gmail.com>
2023-07-31 17:03:38 -0500

    update ranger to only load when executable is installed

--- nvim/lua/plugins/ranger.lua
+++ nvim/lua/plugins/ranger.lua
@@ -1,12 +1,15 @@
 return {
-  "kelly-lin/ranger.nvim",
-  config = function()
-    require("ranger-nvim").setup({ replace_netrw = true })
-    vim.api.nvim_set_keymap("n", "<leader>fi", "", {
-      noremap = true,
-      callback = function()
-        require("ranger-nvim").open(true)
-      end,
-    })
-  end,
+    "kelly-lin/ranger.nvim",
+    config = function()
+        require("ranger-nvim").setup({ replace_netrw = true })
+        vim.api.nvim_set_keymap("n", "<leader>fv", "", {
+            noremap = true,
+            callback = function()
+                require("ranger-nvim").open(true)
+            end,
+        })
+    end,
+    cond = function()
+        return vim.fn.executable 'ranger' == 1
+    end,
 }
