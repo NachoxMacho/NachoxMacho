e0ae246168f880b0e99a0eba78c2478d14e7f36f
Rob Owens <rtowens17@gmail.com>
2023-08-01 00:14:42 -0500

    add telescope undo search

---
+++ nvim/lua/plugins/telescope-undo.lua
@@ -0,0 +1,17 @@
+return {
+    'debugloop/telescope-undo.nvim',
+    dependencies = { 'nvim-telescope/telescope.nvim' },
+    config = function ()
+        require('telescope').setup({
+            extensions = {
+                undo = {
+
+                }
+            }
+        })
+        require('telescope').load_extension('undo')
+    end,
+    keys = {
+        { '<leader>fu', '<cmd>Telescope undo<cr>', desc = 'Undo' }
+    }
+}
