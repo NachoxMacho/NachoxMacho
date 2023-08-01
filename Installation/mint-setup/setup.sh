9ec9ccae88d92207dfe42d077ef28ec5210a47f1
Rob Owens <rtowens17@gmail.com>
2023-07-31 21:29:33 -0500

    add install setups for ranger and bat

--- Installation/mint-setup/setup.sh
+++ Installation/mint-setup/setup.sh
@@ -55,3 +55,16 @@ git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 sudo apt install tmux
 mkdir ~/.config/tmux
 ln -s "$(pwd)/tmux/tmux.conf" "${HOME}/.config/tmux/tmux.conf"
+
+# Ranger
+sudo apt install ranger
+git clone https://github.com/alexanderjeurissen/ranger_devicons ~/.config/ranger/plugins/ranger_devicons
+ln -s "$(pwd)/ranger/rc.conf" "${HOME}/.config/ranger/rc.conf"
+
+# Bat (Ranger preview)
+sudo apt install bat
+mkdir -p "${HOME}/.local/bin"
+ln -s /usr/bin/batcat "${HOME}/.local/bin/bat"
+mkdir "${HOME}/.config/bat"
+ln -s "$(pwd)/bat/config" "${HOME}/.config/bat/config"
+ln -s "$(pwd)/bat/themes" "${HOME}/.config/bat/themes"
