07546f4b329cff9381ac976692a5c1109e33a96f
Rob Owens <rob.owens@rsmus.com>
2023-08-01 12:54:34 +0000

    cleanup and add execute line keybind

--- nvim/lua/plugins/toggleterm.lua
+++ nvim/lua/plugins/toggleterm.lua
@@ -9,16 +9,7 @@ return {
     keys = {
         { '<C-j>', ':ToggleTerm<cr>', mode = 'n', desc = 'Launch Terminal' },
         { '<C-j>', '<C-\\><C-n>:ToggleTerm<cr>', mode = 't', desc = 'Close Terminal' },
-        { '<Esc>', '<C-\\><C-n>', mode = 't', desc = 'Esc Terminal Mode' }
+        { '<Esc>', '<C-\\><C-n>', mode = 't', desc = 'Esc Terminal Mode' },
+        { '<leader>tx', '<cmd>ToggleTermSendCurrentLine<cr>', desc = 'Execute Line' }
     }
 }
-
--- require('toggleterm').setup{
---     direction = 'float',
---     start_in_insert = true,
---     persist_mode = false,
--- }
-
--- vim.keymap.set('n', '<C-j>', ':ToggleTerm<cr>')
--- vim.keymap.set('t', '<C-j>', '<C-\\><C-n>:ToggleTerm<cr>')
--- vim.keymap.set('t', '<Esc>', '<C-\\><C-n>')
