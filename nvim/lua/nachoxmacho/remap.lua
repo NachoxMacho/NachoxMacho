4dc70a30ced0d98d7b387ab51cb7a134bec31cbb
Rob Owens <rob.owens@rsmus.com>
2023-08-09 15:09:24 +0000

    fix issues with keymaps

--- nvim/lua/nachoxmacho/remap.lua
+++ nvim/lua/nachoxmacho/remap.lua
@@ -14,10 +14,10 @@ vim.keymap.set('i', '<A-up>', '<Esc><cmd>m -2 <enter>i', { desc = 'Move line up'
 vim.keymap.set('i', '<A-down>', '<Esc><cmd>m +1 <enter>i', { desc = 'Move line down' })
 
 vim.keymap.set('i', '<A-S-up>', '<Esc><cmd>t-1<cr>a', { desc = 'Copy line up'})
-vim.keymap.set({'n', 'v'}, '<A-S-up>', '<cmd>t-1<cr>', { desc = 'Copy line up'})
+vim.keymap.set({'n', 'v'}, '<A-S-up>', ':t-1<cr>', { desc = 'Copy line up'})
 
 vim.keymap.set('i', '<A-S-down>', '<Esc><cmd>t.<cr>a', { desc = 'Copy line down'})
-vim.keymap.set({'n', 'v'}, '<A-S-down>', '<cmd>t.<cr>', { desc = 'Copy line down'})
+vim.keymap.set({'n', 'v'}, '<A-S-down>', ':t.<cr>', { desc = 'Copy line down'})
 
 vim.keymap.set('n', '<C-S-left>', '<cmd>bnext<cr>', { desc = 'Next buffer' })
 vim.keymap.set('n', '<C-S-right>', '<cmd>bprevious<cr>', { desc = 'Prev buffer'})
@@ -53,11 +53,11 @@ vim.keymap.set('n', '<leader>p', '"0p', { desc = 'Last yank' })
 
 vim.keymap.set('v', '<leader>y', '"+y', { desc = 'System Copy'})
 
-vim.keymap.set('n', '<leader>mf', '<cmd>%s/', { desc = 'Replace in current file' })
-vim.keymap.set('n', '<leader>mc', '<cmd>s//g<left><left>', { desc = 'Replace in current file' })
+vim.keymap.set('n', '<leader>mf', ':%s/', { desc = 'Replace in current file' })
+vim.keymap.set('n', '<leader>mc', ':s//g<left><left>', { desc = 'Replace in current file' })
 
-vim.keymap.set('n', '<leader>fc', '<cmd>e %:h/',{  desc = 'Create File' })
-vim.keymap.set('n', '<leader>fn', '<cmd>e ', { desc = 'Create File' })
+vim.keymap.set('n', '<leader>fc', ':e %:h/',{  desc = 'Create File' })
+vim.keymap.set('n', '<leader>fn', ':e ', { desc = 'Create File' })
 
 local diagnostics_active = true
 vim.keymap.set('n', '<leader>d', function()
