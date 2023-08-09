7837bbef7d9c3c79d01f4ec5e476173f4486c14b
Rob Owens <rob.owens@rsmus.com>
2023-08-09 15:10:04 +0000

    remove excess option

--- nvim/lua/nachoxmacho/set.lua
+++ nvim/lua/nachoxmacho/set.lua
@@ -54,8 +54,6 @@ vim.opt.mouse = 'a'
 -- When breaking a line, indent the following line
 vim.opt.breakindent = true
 
-vim.opt.updatetime = 250
-
 vim.opt.completeopt = 'menuone,noselect'
 
 vim.g.mapleader = " "
