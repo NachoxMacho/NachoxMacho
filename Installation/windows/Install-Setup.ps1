24224ddf1092bf1237ebe6370898d3086b3bafe9
Rob Owens <rob.owens@rsmus.com>
2023-08-09 12:35:59 +0000

    add openconsole setup instructions

--- Installation/windows/Install-Setup.ps1
+++ Installation/windows/Install-Setup.ps1
@@ -1,1 +1,4 @@
 mklink /D "$($PWD)/nvim" "$($env:LocalAppData)/nvim"
+
+# Setup OpenConsole to replace conhost
+# Follow instructions: https://github.com/alacritty/alacritty/issues/1663#issuecomment-644739799
