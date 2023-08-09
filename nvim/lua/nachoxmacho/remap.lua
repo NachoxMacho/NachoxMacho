3568785411c6121085327e2240da8bdc0d981dc0
Rob Owens <rob.owens@rsmus.com>
2023-08-09 12:45:17 +0000

    convert : to <cmd> and fix buffer map in insert mode

--- nvim/lua/nachoxmacho/remap.lua
+++ nvim/lua/nachoxmacho/remap.lua
@@ -1,27 +1,27 @@
 vim.g.mapleader = ' '
 
-vim.keymap.set("n", "<leader>s", ":update<cr>", { desc = 'Save File' } )
-vim.keymap.set('n', '<leader>q', ':q<cr>', { desc = 'Quit File (No Save)' } )
+vim.keymap.set("n", "<leader>s", "<cmd>update<cr>", { desc = 'Save File' } )
+vim.keymap.set('n', '<leader>q', '<cmd>q<cr>', { desc = 'Quit File (No Save)' } )
 vim.keymap.set('n', '<leader>w', '<c-w>', { desc = 'Window Mode' } )
 
-vim.keymap.set("n", "<A-up>", ":m -2<CR>", { desc = 'Move selection up' })
-vim.keymap.set("n", "<A-down>", ":m +1<CR>", { desc = 'Move selection down' })
+vim.keymap.set("n", "<A-up>", "<cmd>m -2<CR>", { desc = 'Move selection up' })
+vim.keymap.set("n", "<A-down>", "<cmd>m +1<CR>", { desc = 'Move selection down' })
 
-vim.keymap.set("v", "<A-up>", ":m '<-2<CR>gv=gv", { desc = 'Move selection up' })
-vim.keymap.set("v", "<A-down>", ":m '>+1<CR>gv=gv", { desc = 'Move selection down' })
+vim.keymap.set("v", "<A-up>", "<cmd>m '<-2<CR>gv=gv", { desc = 'Move selection up' })
+vim.keymap.set("v", "<A-down>", "<cmd>m '>+1<CR>gv=gv", { desc = 'Move selection down' })
 
-vim.keymap.set('i', '<A-up>', '<Esc>:m -2 <enter>i', { desc = 'Move line up' })
-vim.keymap.set('i', '<A-down>', '<Esc>:m +1 <enter>i', { desc = 'Move line down' })
+vim.keymap.set('i', '<A-up>', '<Esc><cmd>m -2 <enter>i', { desc = 'Move line up' })
+vim.keymap.set('i', '<A-down>', '<Esc><cmd>m +1 <enter>i', { desc = 'Move line down' })
 
-vim.keymap.set('i', '<A-S-up>', '<Esc>:t-1<cr>a', { desc = 'Copy line up'})
-vim.keymap.set({'n', 'v'}, '<A-S-up>', ':t-1<cr>', { desc = 'Copy line up'})
+vim.keymap.set('i', '<A-S-up>', '<Esc><cmd>t-1<cr>a', { desc = 'Copy line up'})
+vim.keymap.set({'n', 'v'}, '<A-S-up>', '<cmd>t-1<cr>', { desc = 'Copy line up'})
 
-vim.keymap.set('i', '<A-S-down>', '<Esc>:t.<cr>a', { desc = 'Copy line down'})
-vim.keymap.set({'n', 'v'}, '<A-S-down>', ':t.<cr>', { desc = 'Copy line down'})
+vim.keymap.set('i', '<A-S-down>', '<Esc><cmd>t.<cr>a', { desc = 'Copy line down'})
+vim.keymap.set({'n', 'v'}, '<A-S-down>', '<cmd>t.<cr>', { desc = 'Copy line down'})
 
-vim.keymap.set('n', '<C-S-left>', ':bnext<cr>', { desc = 'Next buffer' })
-vim.keymap.set('n', '<C-S-right>', ':bprevious<cr>', { desc = 'Prev buffer'})
-vim.keymap.set('n', '<C-x>', ':bd<cr>', { desc = 'Close buffer' })
+vim.keymap.set('n', '<C-S-left>', '<cmd>bnext<cr>', { desc = 'Next buffer' })
+vim.keymap.set('n', '<C-S-right>', '<cmd>bprevious<cr>', { desc = 'Prev buffer'})
+vim.keymap.set('n', '<C-x>', '<cmd>bd<cr>', { desc = 'Close buffer' })
 vim.keymap.set('n', '<C-S-x>', '<C-w>o', { desc = 'Close Other windows'})
 
 vim.keymap.set({'n', 'i' }, '<C-left>', '<Esc><C-w><left>', { desc = 'Window Focus Left' })
@@ -29,8 +29,8 @@ vim.keymap.set({'n', 'i' }, '<C-right>', '<Esc><C-w><right>', { desc = 'Window F
 vim.keymap.set({'n', 'i' }, '<C-up>', '<Esc><C-w><up>', { desc = 'Window Focus Up' })
 vim.keymap.set({'n', 'i' }, '<C-down>', '<Esc><C-w><down>', { desc = 'Window Focus Down' })
 
-vim.keymap.set('i', '<C-left>', '<Esc>:bnext<cr>', { desc = 'Next buffer' })
-vim.keymap.set('i', '<C-right>', '<Esc>:bnext<cr>', { desc = 'Previous buffer' })
+vim.keymap.set('i', '<C-S-left>', '<Esc><cmd>bnext<cr>', { desc = 'Next buffer' })
+vim.keymap.set('i', '<C-S-right>', '<Esc><cmd>bnext<cr>', { desc = 'Previous buffer' })
 
 vim.keymap.set({'n', 'i', 'v'}, '<C-S-up>', '<C-u>zz', { desc = 'Page Up' })
 vim.keymap.set({'n', 'i', 'v'}, '<C-S-down>', '<C-d>zz', { desc = 'Page Down' })
@@ -45,19 +45,19 @@ vim.keymap.set('n', '<tab>', '<Nop>', { desc = 'Do nothing'})
 vim.keymap.set('x', '<tab>', '<Nop>', { desc = 'Do nothing'})
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 
-vim.keymap.set('n', '<leader>>', ':.><cr>')
-vim.keymap.set('n', '<leader><', ':.<<cr>')
+vim.keymap.set('n', '<leader>>', '<cmd>.><cr>')
+vim.keymap.set('n', '<leader><', '<cmd>.<<cr>')
 
 vim.keymap.set('n', '<C-p>', '"*p', { desc = 'Star Paste' })
 vim.keymap.set('n', '<leader>p', '"0p', { desc = 'Last yank' })
 
 vim.keymap.set('v', '<leader>y', '"+y', { desc = 'System Copy'})
 
-vim.keymap.set('n', '<leader>mf', ':%s/', { desc = 'Replace in current file' })
-vim.keymap.set('n', '<leader>mc', ':s//g<left><left>', { desc = 'Replace in current file' })
+vim.keymap.set('n', '<leader>mf', '<cmd>%s/', { desc = 'Replace in current file' })
+vim.keymap.set('n', '<leader>mc', '<cmd>s//g<left><left>', { desc = 'Replace in current file' })
 
-vim.keymap.set('n', '<leader>fc', ':e %:h/',{  desc = 'Create File' })
-vim.keymap.set('n', '<leader>fn', ':e ', { desc = 'Create File' })
+vim.keymap.set('n', '<leader>fc', '<cmd>e %:h/',{  desc = 'Create File' })
+vim.keymap.set('n', '<leader>fn', '<cmd>e ', { desc = 'Create File' })
 
 local diagnostics_active = true
 vim.keymap.set('n', '<leader>d', function()
