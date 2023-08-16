3b683a4e84821b5983beb1d31bf645772dc3e30e
Rob Owens <rob.owens@rsmus.com>
2023-08-16 13:10:07 +0000

    remove excess code from browse file

--- nvim/lua/plugins/telescope-browse.lua
+++ nvim/lua/plugins/telescope-browse.lua
@@ -12,17 +12,3 @@ return {
         { '<leader>fv', ':Telescope file_browser<cr>', desc = 'File Browser' }
     }
 }
-
--- local fb_actions = require 'telescope._extensions.file_browser.actions'
-
--- require('telescope').setup {
---     extensions = {
---         file_browser = {
-
---         }
---     }
--- }
-
--- require('telescope').load_extension 'file_browser'
-
--- vim.keymap.set('n', '<leader>fv', ':Telescope file_browser<cr>', { desc = 'File Browser' })
