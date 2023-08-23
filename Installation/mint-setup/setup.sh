b6659f702f907811223693feb780e42af1ae32b8
Rob Owens <rob.owens@rsmus.com>
2023-08-23 12:51:58 +0000

    fix typo in install script

--- Installation/mint-setup/setup.sh
+++ Installation/mint-setup/setup.sh
@@ -86,7 +86,7 @@ ln -s "$(pwd)/zsh/.zshrc" "${HOME}/.zshrc"
 
 # Kubectl Setup
 sudo apt update
-sudo apt instaall ca-certificates curl
+sudo apt install ca-certificates curl
 curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-archive-keyring.gpg
 echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo apt update
