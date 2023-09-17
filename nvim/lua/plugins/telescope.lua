e06d55938fcad7db2d9fbbd1faf8cd3c2cf2639c
Rob Owens <rtowens17@gmail.com>
2023-09-16 23:56:11 -0500

    update telescope to allow switching previewers

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,18 +1,27 @@
 return {
     'nvim-telescope/telescope.nvim',
-    dependencies = { 'nvim-lua/plenary.nvim'},
+    dependencies = { 'nvim-lua/plenary.nvim' },
     tag = '0.1.2',
-    config = function ()
-        require('telescope').setup({})
+    config = function()
+        require('telescope').setup({
+            defaults = {
+                mappings = {
+                    i = {
+                        ["<C-s>"] = require('telescope.actions').cycle_previewers_next,
+                        ["<C-a>"] = require('telescope.actions').cycle_previewers_prev,
+                    }
+                }
+            }
+        })
     end,
     keys = {
-        { '<leader>ff', ':Telescope find_files<cr>', desc = 'Files' },
+        { '<leader>ff', ':Telescope find_files<cr>',                                 desc = 'Files' },
         { '<leader>fa', '<cmd> Telescope find_files no_ignore=true hidden=true<cr>', desc = 'All Files' },
-        { '<leader>fg', ':Telescope git_files<cr>', desc = 'Git Files' },
-        { '<leader>fs', ':Telescope live_grep<cr>', desc = 'Search' },
-        { '<leader>vh', ':Telescope help_tags<cr>', desc = 'Help' },
-        { '<leader>fb', ':Telescope buffers<cr>', desc = 'Buffers' },
-        { '<leader>fw', ':Telescope git_status<cr>', desc = 'Git Working Files (Status)' },
-        { '<leader>ft', '<cmd>Telescope tags<cr>', desc = 'Tags'}
+        { '<leader>fg', ':Telescope git_files<cr>',                                  desc = 'Git Files' },
+        { '<leader>fs', ':Telescope live_grep<cr>',                                  desc = 'Search' },
+        { '<leader>vh', ':Telescope help_tags<cr>',                                  desc = 'Help' },
+        { '<leader>fb', ':Telescope buffers<cr>',                                    desc = 'Buffers' },
+        { '<leader>fw', ':Telescope git_status<cr>',                                 desc = 'Git Working Files (Status)' },
+        { '<leader>ft', '<cmd>Telescope tags<cr>',                                   desc = 'Tags' }
     }
 }
