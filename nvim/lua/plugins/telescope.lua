25e35b425323ad823fdf0f359b1446d08d7952d8
Rob Owens <rtowens17@gmail.com>
2023-09-21 04:56:58 +0000

    Update telescope.lua to new tag to fix render issue

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,7 +1,7 @@
 return {
     'nvim-telescope/telescope.nvim',
     dependencies = { 'nvim-lua/plenary.nvim' },
-    tag = '0.1.2',
+    tag = '0.1.x',
     config = function()
         require('telescope').setup({
             defaults = {
