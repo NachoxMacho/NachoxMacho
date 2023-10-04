c83d1c2cd2cca7d8e9cbe38ff9ca1e3842c5f29d
Rob Owens <rtowens17@gmail.com>
2023-10-03 23:06:29 -0500

    update ranger plugin to open in floating window

--- nvim/lua/plugins/ranger.lua
+++ nvim/lua/plugins/ranger.lua
@@ -1,7 +1,16 @@
 return {
     "kelly-lin/ranger.nvim",
     config = function()
-        require("ranger-nvim").setup({ replace_netrw = true })
+        require("ranger-nvim").setup({
+            replace_netrw = true,
+            ui = {
+                border = "single",
+                height = 0.9,
+                width = 0.9,
+                -- x = 0.5,
+                -- y = 0.5
+            }
+        })
         vim.api.nvim_set_keymap("n", "<leader>fv", "", {
             noremap = true,
             callback = function()
