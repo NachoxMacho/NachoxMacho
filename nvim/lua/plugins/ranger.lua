8c19cb4729c8a1a483e090ed8677b57204f467bd
Rob Owens <rtowens17@gmail.com>
2023-10-03 23:06:53 -0500

    remove excess code block

--- nvim/lua/plugins/ranger.lua
+++ nvim/lua/plugins/ranger.lua
@@ -7,8 +7,6 @@ return {
                 border = "single",
                 height = 0.9,
                 width = 0.9,
-                -- x = 0.5,
-                -- y = 0.5
             }
         })
         vim.api.nvim_set_keymap("n", "<leader>fv", "", {
