4545968801e51ac454601927a1dba361923137c5
Rob Owens <rtowens17@gmail.com>
2023-10-09 20:09:47 -0500

    add go installation and c++ dependencies

--- Installation/mint-setup/setup.sh
+++ Installation/mint-setup/setup.sh
@@ -109,3 +109,12 @@ sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/microsoft.gpg] h
 sudo apt update && sudo apt install -y powershell
 
 sudo apt install zoxide
+
+# C++ Dev dependencies
+sudo apt install libc++-dev
+
+# Go install
+wget -OL https://go.dev/dl/go1.21.2.linux-amd64.tar.gz
+rm -rf /usr/local/go 
+sudo tar -C /usr/local -xzf go1.21.2.linux-amd64.tar.gz
+rm go1.21.2.linux-amd64.tar.gz
--- bash/.bashrc
+++ bash/.bashrc
@@ -138,3 +138,6 @@ case ":$PATH:" in
   *) export PATH="$PNPM_HOME:$PATH" ;;
 esac
 # pnpm end
+
+# go
+export PATH=$PATH:/usr/local/go/bin
