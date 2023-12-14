b26a5ed5333ab99ba07cd00a71bdf440dcc1b5b9
Rob Owens <rob.owens@rsmus.com>
2023-12-14 13:35:46 +0000

    update duplicate keybinds to function in normal mode

--- nvim/lua/plugins/duplicate.lua
+++ nvim/lua/plugins/duplicate.lua
@@ -2,8 +2,8 @@ return {
     'hinell/duplicate.nvim',
     dependencies = { },
     keys = {
-        { '<S-A-Up>',      ':LineDuplicate -1<CR>',   mode = 'n' },
-        { '<S-A-Down>',    ':LineDuplicate 1<CR>',    mode = 'n' },
+        { '<S-A-Up>',      '<cmd>LineDuplicate -1<cr>',   mode = 'n' },
+        { '<S-A-Down>',    '<cmd>LineDuplicate +1<cr>',    mode = 'n' },
 
         -- Visual-mode commands
         { '<S-A-Up>',      ':VisualDuplicate -1<CR>',  mode = 'x' },
