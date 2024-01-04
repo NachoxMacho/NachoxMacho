4839902f8941e3c23ec8b645ade1840f35208a91
Rob Owens <rob.owens@rsmus.com>
2024-01-04 21:48:01 +0000

    remove harpoon and add extra terminals

--- nvim/lua/plugins/toggleterm.lua
+++ nvim/lua/plugins/toggleterm.lua
@@ -8,6 +8,10 @@ return {
     },
     keys = {
         { '<C-j>', ':ToggleTerm<cr>', mode = 'n', desc = 'Launch Terminal' },
+        { '<leader>ju', ':ToggleTerm 1<cr>', mode = 'n', desc = 'Launch Terminal' },
+        { '<leader>ji', ':ToggleTerm 2<cr>', mode = 'n', desc = 'Launch Terminal' },
+        { '<leader>jo', ':ToggleTerm 3<cr>', mode = 'n', desc = 'Launch Terminal' },
+        { '<leader>jp', ':ToggleTerm 4<cr>', mode = 'n', desc = 'Launch Terminal' },
         { '<C-j>', '<C-\\><C-n>:ToggleTerm<cr>', mode = 't', desc = 'Close Terminal' },
         { '<Esc>', '<C-\\><C-n>', mode = 't', desc = 'Esc Terminal Mode' },
         { '<leader>tx', '<cmd>ToggleTermSendCurrentLine<cr>', desc = 'Execute Line' }
