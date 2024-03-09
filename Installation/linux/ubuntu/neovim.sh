a45f1cf401bc37f255be32c666493f53c804053c
Rob Owens <rtowens17@gmail.com>
2024-03-09 09:06:13 -0600

    switch nvim install to app image for compatibility

--- Installation/linux/ubuntu/neovim.sh
+++ Installation/linux/ubuntu/neovim.sh
@@ -1,5 +1,10 @@
 # Install NeoVim
-sudo snap install neovim --channel=edge
+# sudo snap install neovim --channel=edge
+
+rm /usr/local/bin/nvim
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+chmod u+x nvim.appimage
+sudo mv nvim.appimage /usr/local/bin/nvim
 
 # Needed for telescope in nvim
 sudo apt install ripgrep
