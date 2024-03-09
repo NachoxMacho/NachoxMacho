5257b7aefdbbafadade2e80c72c2952ee7d86157
Rob Owens <rtowens17@gmail.com>
2024-03-09 09:42:13 -0600

    formatting changes

--- nvim/lua/nachoxmacho/set.lua
+++ nvim/lua/nachoxmacho/set.lua
@@ -8,6 +8,7 @@ vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
+
 vim.opt.splitright = true
 vim.opt.splitbelow = true
 
@@ -22,6 +23,7 @@ vim.opt.fixeol = false
 
 vim.opt.swapfile = false
 vim.opt.backup = false
+
 if vim.fn.has('win64') == 1 or vim.fn.has('win32') == 1 or vim.fn.has('win16') == 1 then
     -- vim.opt.undodir = os.getenv('LocalAppData') .. '/nvim-data/undodir'
     local powershell_options = {
