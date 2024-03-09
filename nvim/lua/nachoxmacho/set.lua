2c10bf1b787cf6f4ec007cfba7977a063bbf4f86
Rob Owens <rtowens17@gmail.com>
2024-03-09 09:58:04 -0600

    add highlight to yanking

--- nvim/lua/nachoxmacho/set.lua
+++ nvim/lua/nachoxmacho/set.lua
@@ -74,3 +74,12 @@ vim.api.nvim_create_autocmd("BufWritePre", {
     pattern = "*",
     command = [[%s/\s\+$//e]],
 })
+
+-- Highlight when yanking (copying) text
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
