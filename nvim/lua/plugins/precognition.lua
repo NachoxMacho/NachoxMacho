b00f4323c3debb8bf357eceb706b6b984e6e39e2
Rob Owens <rtowens17@gmail.com>
2024-06-02 21:31:16 -0500

    fix rabbit and pre-cognition setup

--- nvim/lua/plugins/precognition.lua
+++ nvim/lua/plugins/precognition.lua
@@ -6,9 +6,14 @@ return {
         hints = {
             Caret = { text = "^", prio = 0 },
             Dollar = { text = "$", prio = 0 },
+            Zero = { text = "0", prio = 0 },
+            MatchingPair = { text = "%", prio = 0 },
             w = { text = "w", prio = 0 },
             b = { text = "b", prio = 0 },
             e = { text = "e", prio = 0 },
+            E = { text = "E", prio = 0 },
+            B = { text = "B", prio = 0 },
+            W = { text = "W", prio = 0 },
         },
         gutterHints = {
             --prio is not currentlt used for gutter hints
--- nvim/lua/plugins/rabbit.lua
+++ nvim/lua/plugins/rabbit.lua
@@ -1,6 +1,6 @@
 return {
     'voxelprismatic/rabbit.nvim',
     config = function()
-        require('rabbit').setup('<leader>r')
+        require('rabbit').setup({})
     end,
 }
