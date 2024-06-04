89686695507819bdd5a119fc1bdb4160d3c39514
Rob Owens <rob.owens@rsmus.com>
2024-06-04 07:17:52 -0500

    add netrw default style

--- nvim/lua/nachoxmacho/set.lua
+++ nvim/lua/nachoxmacho/set.lua
@@ -4,7 +4,7 @@ vim.opt.relativenumber = false
 -- NetRW settings
 vim.g.netrw_keepdir = 1
 vim.g.netrw_localcopydircommand = 'cp -r'
-
+vim.g.netrw_liststyle = 3
 
 -- vim.g.loaded_netrw = 1
 -- vim.g.loaded_netrwPlugin = 1
