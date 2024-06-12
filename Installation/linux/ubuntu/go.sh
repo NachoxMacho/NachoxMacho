84985b19425db730167cfe2f33b64758000166c2
Rob Owens <rtowens17@gmail.com>
2024-06-12 13:42:09 -0500

    update go version

--- Installation/linux/ubuntu/go.sh
+++ Installation/linux/ubuntu/go.sh
@@ -2,7 +2,7 @@
 sudo apt install libc++-dev
 
 # Go install
-wget https://go.dev/dl/go1.22.1.linux-amd64.tar.gz
+wget https://go.dev/dl/go1.22.4.linux-amd64.tar.gz
 sudo rm -rf /usr/local/go
-sudo tar -C /usr/local -xzf go1.22.1.linux-amd64.tar.gz
-sudo rm go1.22.1.linux-amd64.tar.gz
+sudo tar -C /usr/local -xzf go1.22.4.linux-amd64.tar.gz
+sudo rm go1.22.4.linux-amd64.tar.gz
