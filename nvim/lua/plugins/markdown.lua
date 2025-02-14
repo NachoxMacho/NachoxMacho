57a8d33976478a1d33e001b0aa867345c318f281
Rob Owens <rtowens17@gmail.com>
2025-02-14 01:36:06 -0600

    add markdown rendering plugin

---
+++ nvim/lua/plugins/markdown.lua
@@ -0,0 +1,15 @@
+return {
+    'MeanderingProgrammer/render-markdown.nvim',
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+    config = function ()
+        require('render-markdown').setup({
+            paragraph = {
+                enabled = true,
+                left_margin = 0,
+                min_width = 0,
+            },
+        })
+    end
+}
