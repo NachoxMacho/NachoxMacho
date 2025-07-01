0dc3a84a5d709fc789edc234a3b22e9fe4860373
Rob Owens <rtowens17@gmail.com>
2025-06-30 20:28:44 -0500

    feat: start adding ftplugin configuration for language specific options

---
+++ nvim/after/ftplugin/go.lua
@@ -0,0 +1,2 @@
+vim.keymap.set('n', '<Leader>ni', '<cmd>GoImports<cr>', { desc = 'Imports', remap = false, silent = true })
+vim.keymap.set('n', '<leader>nt', '<cmd>GoModTidy<cr>', { desc = 'Go Mod Tidy', remap = false, silent = true })
