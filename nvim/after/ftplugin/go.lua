fe144189f37601e18295649894574e7a7792ca9d
Rob Owens <rtowens17@gmail.com>
2026-04-19 09:41:24 -0500

    minor tweaks to sessionizer and go mapping

--- nvim/after/ftplugin/go.lua
+++ nvim/after/ftplugin/go.lua
@@ -2,7 +2,9 @@ vim.keymap.set('n', '<Leader>ni', '<cmd>GoImports<cr>', { desc = 'Imports', rema
 vim.keymap.set('n', '<leader>nc', '<cmd>GoCmt<cr>', { desc = 'Go Doc Comment', remap = false, silent = true, buffer = 0 })
 vim.keymap.set('n', '<leader>ng', '<cmd>GoTestAdd<cr>',
   { desc = 'Go Add Test', remap = false, silent = true, buffer = 0 })
+vim.keymap.set('n', '<leader>nf', '<cmd>GoTestFile<cr>',
+  { desc = 'Go Test File', remap = false, silent = true, buffer = 0 })
 vim.keymap.set('n', '<leader>na', '<cmd>GoAlt<cr>',
   { desc = 'Go Alternate file', remap = false, silent = true, buffer = 0 })
-vim.keymap.set('n', "<leader>nl", function() require("neotest").run.run_last() end,
-  { desc = "Rerun last test", remap = false, silent = true, buffer = 0 })
+-- vim.keymap.set('n', "<leader>nl", function() require("neotest").run.run_last() end,
+--   { desc = "Rerun last test", remap = false, silent = true, buffer = 0 })
